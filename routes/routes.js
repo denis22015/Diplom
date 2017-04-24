09e1ba4cf849600c13b77d827ce4ad4ca4d345d5
Denis22015 <denis22015@gmail.com>
2017-04-25 00:13:57 +0100

    evening test

--- routes/routes.js
+++ routes/routes.js
@@ -56,9 +56,6 @@ module.exports = function(app){
 		// 			}
 		// 	res.end("OK")
 		// })
-
-		const lat = elem.lat
-		const lng = elem.lng
 		const id = elem.id
 		request("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+  /*language=uk*/
 			"&key=AIzaSyBzMpfnfVh5RyH-tb-xw5w4YoqDFcViC08", function(err,data,body) {
